baacd4ae18398ce8a5029104af85446f5d49803b
fermentday <348207971@qq.com>
2024-08-22 01:20:11 +0100

    fix path

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
@@ -8,10 +8,10 @@ const port = 3000;
 This is necessary because when a client (such as a web browser) requests a file like style.css or dist/script.js, 
 the server needs to know where to find these files and how to serve them.
 */
-app.use(express.static('.'));
+app.use(express.static("."));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: '.' });
+app.get("/", (req, res) => {
+  res.sendFile("dist/index.html", { root: "." });
 });
 
 app.listen(port, () => {
