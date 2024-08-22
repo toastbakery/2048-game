016c9b82c72b199bab663061307f4aab4d2da951
fermentday <348207971@qq.com>
2024-08-22 02:09:00 +0100

    Fix paths for GitHub Pages

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 This is necessary because when a client (such as a web browser) requests a file like style.css or dist/script.js, 
 the server needs to know where to find these files and how to serve them.
 */
-app.use(express.static("."));
+app.use(express.static("./dist"));
 
 app.get("/", (req, res) => {
   res.sendFile("dist/index.html", { root: "." });
